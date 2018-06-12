61a4d227d89b572e3070632827937e171be7e8f2
Sarah Manning <sarahnmanning@gmail.com>
2018-06-11 20:42:36 -0400

    wrote the functions of triple working with pythagorean theorem and sub function with several logs

--- functions/starter.js
+++ functions/starter.js
@@ -22,6 +22,25 @@ function length(word) {
     return word.length;
 }
 
+/**
+ * @param a - number of the small side of a triangle
+ * @param b - number of the middle side of a triangle
+ * @param c - number of the hypotenuse
+ * @return boolean true if a^2 + b^2 == c^2
+ */
+function triple(a, b, c) {
+    return a * a + b * b == c * c;
+}
+
+/**
+ * @param x first number
+ * @param y second number
+ * @return difference of x and y (x - y)
+ */
+function sub(x,  y) {
+        return x - y;
+}
+
 const result = concat("Yay", "woooooo!!!");
 console.log(result);
 const result2 = concat(10, 3);
@@ -43,3 +62,10 @@ console.log(lengthOfCheese);
 
 const crazyWord = length("supercalifragilisticexpialidocious");
 console.log(crazyWord);
+
+console.log("Is 3, 4, 5, a triple?", triple(3, 4, 5));
+console.log("Is 5, 9, 16 a triple?", triple(5, 9, 16));
+
+//sub test cases
+console.log('sub(3, 4) should be -1', sub(3, 4));
+console.log('sub(10, 4) should be 6', sub(10, 4));
