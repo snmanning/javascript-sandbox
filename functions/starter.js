f588bf2e6e821f0340b6d3b1a04c2bed99b8a16a
Sarah Manning <sarahnmanning@gmail.com>
2018-06-11 20:09:26 -0400

    wrote more functions as practice for lesson application.

--- functions/starter.js
+++ functions/starter.js
@@ -12,6 +12,16 @@ function fortyTwo() {
     return 42;
 }
 
+// add function
+function add(x, y) {
+    return x + y;
+}
+
+// length function
+function length(word) {
+    return word.length;
+}
+
 const result = concat("Yay", "woooooo!!!");
 console.log(result);
 const result2 = concat(10, 3);
@@ -24,3 +34,12 @@ console.log(product2);
 
 console.log(fortyTwo());
 console.log(fortyTwo("cheese"));
+
+const sum = add(7, 3);
+console.log(sum);
+
+const lengthOfCheese = length("cheese");
+console.log(lengthOfCheese);
+
+const crazyWord = length("supercalifragilisticexpialidocious");
+console.log(crazyWord);
