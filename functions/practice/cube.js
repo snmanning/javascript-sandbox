9bd0a9d4a44f69606cae289a7ce32a0297cbd749
Sarah Manning <sarahnmanning@gmail.com>
2018-06-11 21:00:44 -0400

    completion of function exercises for cube.js and square.js

---
+++ functions/practice/cube.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {number} n some number
+ * @return {number} the cube or (n ^ 3) or ( n * n * n)
+ */
+function cube(n) {
+    return n * n * n;
+}
+
+
+console.log("cube(3) should be 27", cube(3));
+console.log("cube(2) should be 8", cube(2));
+console.log("cube(4) should be 64", cube(4));
---
+++ functions/practice/square.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {number} n 
+ * @return {number} the square of n  or (n ^ 2) or (n * n)
+ */
+function square(n) {
+    return n * n;
+}
+
+
+console.log('square(2) is 4', square(2));
+console.log('square(3) is 9', square(3));
+console.log('square(4) is 16', square(4));
