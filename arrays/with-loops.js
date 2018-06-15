9c7e610d907788dc26fbb17c4799418bb355be79
Sarah Manning <sarahnmanning@gmail.com>
2018-06-14 20:22:27 -0400

    used for loops to access every elements in 2 sample arrays

---
+++ arrays/with-loops.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3];
+const letters = ['a', 'b', 'c'];
+
+//Exercise: I want to print every element of numbers to the screen (one per line)
+for(let i = 0; i < numbers.length; i += 1) {
+    const element = numbers[i];
+    console.log(element);
+}
+
+//Exercise: I want to print every element of letters to the screen (one per line)
+for(let l = 0; l < letters.length; l += 1) {
+    const element = letters[l];
+    console.log(element);
+}
+
