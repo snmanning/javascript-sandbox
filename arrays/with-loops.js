802cd3d316a9b41963550eaa4adb2127d30f3406
Sarah Manning <sarahnmanning@gmail.com>
2018-06-14 21:10:47 -0400

    worked with filter in arrays

--- arrays/with-loops.js
+++ arrays/with-loops.js
@@ -38,3 +38,23 @@ console.log(doubled);
 //Exercise: I want to add a period, "." to every element of the letters array
 const withPeriod = letters.map((element, index) => '.' + element);
 console.log(withPeriod);  
+
+const otherNumbers = [10, 11, 12, 13, 14];
+//Exercise: I want to find the elements that are divisible by 2
+//hint: % modulo returns the remainder
+const evens = otherNumbers.filter((element) => element % 2 === 0);
+console.log(evens);
+
+const odds = otherNumbers.filter((element) => element % 2 === 1);
+console.log(odds);
+
+//Exersice: I want to find the elements of the words array that are 3 letters long
+const words = ['cat', 'dog', 'cheese', 'ham', 'burrito'];
+
+const three = words.filter((word) => word.length / 3 === 1);
+console.log(three);
+
+const x = words.filter((word) => word.length / 3 === 1)
+                .map((word) => word.length * 3);
+
+         console.log(x);
